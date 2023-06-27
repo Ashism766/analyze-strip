8a074b161f1900862b20ce590dffc8f16817ce23
AshisMaity <maity.ashis651@gmail.com>
2023-06-27 11:47:59 +0530

    add details

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 from app.api import routes
 
@@ -9,6 +12,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(routes.router)
 
+PORT = os.environ.get('PORT') or 8000
+
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=int(PORT))

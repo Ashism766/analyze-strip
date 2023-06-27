fddcacdd7b304b3cb922f335b62cceeade649b04
AshisMaity <maity.ashis651@gmail.com>
2023-06-27 12:17:09 +0530

    add cors

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 load_dotenv()
 import os
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import routes
 
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(routes.router)
